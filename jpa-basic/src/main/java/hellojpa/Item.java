c6294b355f0d5e5780ed568a0497cbdc3b675e70
yusunjo <tnswh2023@gmail.com>
2021-02-18 14:15:10 +0900

    구현 클래스마다 테이블 전략 -- 안쓰임

--- jpa-basic/src/main/java/hellojpa/Item.java
+++ jpa-basic/src/main/java/hellojpa/Item.java
@@ -4,9 +4,10 @@ import javax.persistence.*;
 
 @Entity
 //@Inheritance(strategy = InheritanceType.JOINED)     //이거 없으면 그냥 한 테이블에 다 생김
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //단일 테이블 전략
-@DiscriminatorColumn                           //Dtype까지 생성해줌,   단일테이블 전략에선 꼭 필요
-public class Item {
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //단일 테이블 전략
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)   //구현 테이블 마다 -- Item테이블이 안생김, 안쓰임
+//@DiscriminatorColumn                           //Dtype까지 생성해줌,   단일테이블 전략에선 꼭 필요
+public abstract class Item {
 
     @Id @GeneratedValue
     private Long id;
