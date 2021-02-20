b76d903095210446bb3faa8f53e756c9bb29d21e
yusunjo <tnswh2023@gmail.com>
2021-02-20 17:31:49 +0900

    join

--- JPQL/src/main/java/JPQL/main/JpaMain.java
+++ JPQL/src/main/java/JPQL/main/JpaMain.java
@@ -18,21 +18,38 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(10);
+            member.setTeam(team);
+
             em.persist(member);
 
             em.flush();
             em.clear();
 
-            em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            //내부 조인
+            em.createQuery("select m from Member m inner join m.team t", Member.class)
+                    .getResultList();
+            //외부 조인
+            em.createQuery("select m from Member m left join m.team t", Member.class)
+                    .getResultList();
+            //세타 조인
+            em.createQuery("select m from Member m, Team t where m.username = t.name", Member.class)
                     .getResultList();
 
+            //조인 대상 필터링
+            em.createQuery("select m from Member m left join m.team t on t.name='teamA'", Member.class)
+                    .getResultList();
 
-
+            //연관관계가 없는 엔티티 외부 조인
+            em.createQuery("select m from Member m left join Team t on m.username = t.name", Member.class)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
