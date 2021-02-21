fd98143f0a260bf7590caed81fd4d72357c25221
yusunjo <tnswh2023@gmail.com>
2021-02-21 17:49:49 +0900

    엔티티 직접 사용

--- JPQL/src/main/java/JPQL/main/JpaMain.java
+++ JPQL/src/main/java/JPQL/main/JpaMain.java
@@ -45,42 +45,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-//            String query = "select m From Member m";
-            //패치 조인 -> 한방 쿼리로 그냥 한번에 join해서 다 가져옴 - 지연로딩X
-//            String query = "select m From Member m join fetch m.team";
+            //엔티티를 파라미터로 전달
+//            String query = "select m from Member m where m = :member";
 
-            //컬랙션 패치조인 ->
-//            String query = "select t from Team t join fetch t.members";
-
-            //컬렉션 패치조인 중복 제거
-//            String query = "select distinct t from Team t join fetch t.members";
-
-            //패치 조인의 한계 -> 별칭을 주면X
-            String query = "select t from Team t join fetch t.members m";
-
-            List<Team> resultList = em.createQuery(query, Team.class)
+            String query = "select m from Member m where m.team = :team"
+            em.createQuery(query, Member.class)
+//                    .setParameter("member", member1)
+                    .setParameter("team", teamA)
                     .getResultList();
-
-
-
-//            for (Member member : resultList) {
-//                System.out.println("Member="+ member);
-//            }
-//            for (Member member : resultList) {
-//                System.out.println("Member="+ member.getUsername() + ","+ member.getTeam().getName());
-//                //회원 1, 팀A(SQL)
-//                //회원 2, 팀A(1차 캐시)
-//                //회원 3, 팀B(SQL)
-//
-//                //문제점 - 회원 100명이면 100개 넘게 쿼리가 나가서 문제 -- 페치조인 해야함.
-//            }
-
-            for (Team team : resultList) {
-                System.out.println("team=" + team.getName() + " | " + team.getMembers().size());
-                for (Member member: team.getMembers()) {
-                    System.out.println("-> member = " + member);
-                }
-            }
 
             tx.commit();
         } catch (Exception e) {
