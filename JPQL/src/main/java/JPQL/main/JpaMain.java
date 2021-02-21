8019db9fb7da5fb1872510a1dfb0db60289671fd
yusunjo <tnswh2023@gmail.com>
2021-02-21 17:12:13 +0900

    페치 조인 기본

--- JPQL/src/main/java/JPQL/main/JpaMain.java
+++ JPQL/src/main/java/JPQL/main/JpaMain.java
@@ -19,32 +19,62 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            member.setTeam(team);
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
 
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String Myquery ="select m.username from Member m " +
-                    "join m.team t " +
-                    "join m.orders o where t.name='teamA'";
+//            String query = "select m From Member m";
+            //패치 조인 -> 한방 쿼리로 그냥 한번에 join해서 다 가져옴 - 지연로딩X
+//            String query = "select m From Member m join fetch m.team";
 
-            //t.members -> 컬렉션이여서 탐색 불가능
-//            String query = "select t.members From Team t";
+            //컬랙션 패치조인 ->
+//            String query = "select t from Team t join fetch t.members";
 
-            //탐색하고 싶으면 이렇게 join 해서 해야한다. -- from 절에서 명시적 조인
-            String query = "select m.username From Team t join t.members m";
-
-            em.createQuery(query)
+            //컬렉션 패치조인 중복 제거
+            String query = "select distinct t from Team t join fetch t.members";
+            List<Team> resultList = em.createQuery(query, Team.class)
                     .getResultList();
+
+//            for (Member member : resultList) {
+//                System.out.println("Member="+ member);
+//            }
+//            for (Member member : resultList) {
+//                System.out.println("Member="+ member.getUsername() + ","+ member.getTeam().getName());
+//                //회원 1, 팀A(SQL)
+//                //회원 2, 팀A(1차 캐시)
+//                //회원 3, 팀B(SQL)
+//
+//                //문제점 - 회원 100명이면 100개 넘게 쿼리가 나가서 문제 -- 페치조인 해야함.
+//            }
+
+            for (Team team : resultList) {
+                System.out.println("team=" + team.getName() + " | " + team.getMembers().size());
+                for (Member member: team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
+            }
 
             tx.commit();
         } catch (Exception e) {
