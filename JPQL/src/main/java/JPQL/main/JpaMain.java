f0713fef0e1b2bfb4315e1d7330fff00da5b4bcd
yusunjo <tnswh2023@gmail.com>
2021-02-21 20:37:27 +0900

    벌크 연산

--- JPQL/src/main/java/JPQL/main/JpaMain.java
+++ JPQL/src/main/java/JPQL/main/JpaMain.java
@@ -41,13 +41,18 @@ public class JpaMain {
             member3.setUsername("회원3");
             member3.setTeam(teamB);
             em.persist(member3);
+//
+//            em.flush();
+//            em.clear();
 
-            em.flush();
+            //flush 자동 호출  commit , query 나갈 때 자동 호출
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
             em.clear();
-
-            em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            // em.clear() 을 안했다면 0으로 되어 있음
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
